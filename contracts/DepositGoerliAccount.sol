ad361844cb3c31d3bd1f01db8e452e1f8d222c16
Md Tauhidul Islam <42988001+tauhid92@users.noreply.github.com>
2022-11-09 01:04:43 -0600

    increased scope of the contract

--- contracts/DepositGoerliAccount.sol
+++ contracts/DepositGoerliAccount.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.17.0;
 contract DepositGoerliAccount {
     address public owner;
+    bool public paused;
 
     event DepositEvent(
-        bytes pubkey,
+        address sender,
         bytes amount
     );
 
@@ -13,25 +14,40 @@ contract DepositGoerliAccount {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "You are not the owner");
         _;
     }
 
+    modifier notPaused(){
+        require(paused == false, "Contract Paused");
+        _;
+    }
 
-    
-    function depositGoerli(bytes calldata pubkey) payable external {
-        require(pubkey.length == 48, "DepositContract: invalid pubkey length");
+    function setPaused(bool _paused) public onlyOwner{
+        paused = _paused;
+    }
+   
+    function depositGoerli() payable external {
+        // require(pubkey.length == 48, "DepositContract: invalid pubkey length");
 
         // Check deposit amount
         require(msg.value >= 0.01 ether, "DepositContract: deposit value too low");
 
         uint deposit_amount = msg.value / 1 gwei;
         require(deposit_amount <= type(uint64).max, "DepositContract: deposit value too high");
-
-        
+      
         emit DepositEvent(
-            pubkey,
+            msg.sender,
             abi.encodePacked(deposit_amount)
         );
     }
+
+    function withdrawAllMoney(address payable _to) public onlyOwner notPaused{     
+        _to.transfer(address(this).balance);
+    }
+    
+    function destroySmartContract(address payable _to) public onlyOwner{      
+        selfdestruct(_to);
+    }
+    
 }
