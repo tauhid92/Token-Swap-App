d5c38f927c6ff1f11096bb07d176467b5d7c1a97
Md Tauhidul Islam <tauhidul2011@gmail.com>
2022-11-26 20:44:31 -0600

    init rinkeby withdrawal tests

---
+++ rinkeby-withdrawal-contracts/test/RinkebyWithdrawalTest.ts
@@ -0,0 +1,14 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect, assert } from "chai";
+import { ethers } from "hardhat";
+
+describe("Ownership", () => {});
+
+describe("Limits and balances", () => {});
+
+describe("Pause", () => {});
+
+describe("Events", () => {});
+
+describe("Transfer", () => {});
