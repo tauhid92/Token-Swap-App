e4428dd5e009051812c9e824ba7ba9765e5486b3
Md Tauhidul Islam <tauhidul2011@gmail.com>
2022-12-27 22:48:41 -0500

    added logos

--- Swap-Frontend/src/components/ExchangeForm.vue
+++ Swap-Frontend/src/components/ExchangeForm.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>Exchange Form</h1>
+  <div class="container">
+    <h4 class="dynamic-label">Dynamic Label</h4>
+    <form class="form">
+      <label class="form_input-cover"> </label>
+
+      <label class="form_label">Wallet Address from</label>
+      <label class="form_label">Wallet Address to</label>
+      <button class="form_button">Connect Wallet</button>
+      <button class="form_button">Confirm</button>
+    </form>
+  </div>
 </template>
