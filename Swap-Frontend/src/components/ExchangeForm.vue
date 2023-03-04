16b26373cebc7a01d96e8006175a3a1f6c33a5f1
tauhid92 <tauhidul2011@gmail.com>
2023-03-03 22:05:24 -0500

    modified the input field

--- Swap-Frontend/src/components/ExchangeForm.vue
+++ Swap-Frontend/src/components/ExchangeForm.vue
@@ -3,33 +3,19 @@
   <div class="container">
     <h4 class="dynamic-label">Dynamic Label</h4>
     <form class="form">
-      <label class="form_input-cover">
-        <div class="form_input-cover_span1">
-          <svg>
+      <div class="form_input-block">
+      <div class="form_input-block_icon">
+        <svg>
             <use xlink:href="../assets/sprite.svg#ether-icon"></use>
-          </svg>
-        </div>
-        <div class="form_input-cover_span2">
-          <!-- the span will be a v-if and it will be invisible or stay as a placeholder some how till it is clicked-->
-          <!-- <span class="form_input-cover_span2_label_amount">Amount</span> -->
-          
-          <input
-            id ="amount"
-            class="form_input-cover_span2_input"
-            type="text"
-            autocomplete="off"
-          />
-          <label for="amount" class="form_input-cover_span2_label">
-            <span class="form_input-cover_span2_label_amount">Amount</span>
-          </label>
-        </div>
-        <div class="input-cover__span3">
-          <button class="input-cover__span3__btn">
-            <img src="../assets/exchange-2.svg" class="colorize" />
-          </button>
-        </div>
-      </label>
-
+        </svg>
+      </div>
+      <div class="form_input-block_field">      
+        <label class="form_input-block_field_input-label" for="amount">
+          <span class="form_input-block_field_input-label">Amount</span>
+        </label>
+        <input class="form_input-block_field_input" id="amount" type="text">
+      </div>
+      </div>
       <label class="form_label">Wallet Address from</label>
       <label class="form_label">Wallet Address to</label>
       <button class="form_button">Connect Wallet</button>
